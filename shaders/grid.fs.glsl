494e1e3f4bf6c757a36833b802ea1e5d9226133f
Remi Papillie <remi.papillie@gmail.com>
2023-03-02 01:39:23 +0100

    grid luminance adjustments

--- shaders/grid.fs.glsl
+++ shaders/grid.fs.glsl
@@ -8,7 +8,7 @@ void main()
 {
     float width = 1.5 * max(abs(dFdx(in_world_position.x)), abs(dFdy(in_world_position.x)));
     float alpha = smoothstep(width, 0.0, abs(fract(in_world_position.x + 0.5) - 0.5)) * (1.0 - width);
-    float green = step(abs(in_world_position.x), width);
+    float green = step(abs(in_world_position.x), width) * 0.5; // roughly match luminance of red
 
     width = 1.5 * max(abs(dFdx(in_world_position.y)), abs(dFdy(in_world_position.y)));
     alpha += smoothstep(width, 0.0, abs(fract(in_world_position.y + 0.5) - 0.5)) * (1.0 - width);
@@ -17,6 +17,6 @@ void main()
     alpha = clamp(alpha, 0.0, 1.0);
 
     float intensity = 0.12;
-    float color_intensity = 0.1;
+    float color_intensity = 0.14;
     output_color = vec4(intensity + red * color_intensity, intensity + green * color_intensity, intensity, alpha);
 }
